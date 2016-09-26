60f594fc37f8bc4cd68695bcf01c5afb1c5c5adf
DutchITRecruitment <relocation.aid@gmail.com>
2016-09-26 08:50:12 +0200

    WIP

--- js/app.js
+++ js/app.js
@@ -1,55 +1,59 @@
-angular.module('ShoppingListCheckOff', [])
-    .service('ShoppingListCheckOffService', [ShoppingListCheckOff]);
+(function() {
+    "use strict";
 
-angular.module('ShoppingListCheckOff')
-    .controller('ToBuyShoppingController', ['$scope', 'ShoppingListCheckOffService', ToBuyShoppingController])
-    .controller('AlreadyBoughtShoppingController', ['$scope', 'ShoppingListCheckOffService', AlreadyBoughtShoppingController]);
+    angular.module('ShoppingListCheckOff', [])
+        .service('ShoppingListCheckOffService', [ShoppingListCheckOff]);
+
+    angular.module('ShoppingListCheckOff')
+        .controller('ToBuyShoppingController', ['$scope', 'ShoppingListCheckOffService', ToBuyShoppingController])
+        .controller('AlreadyBoughtShoppingController', ['$scope', 'ShoppingListCheckOffService', AlreadyBoughtShoppingController]);
 
 
-function ShoppingListCheckOff($scope) {
-    this.items = [{
-        "name": "cookies",
-        "quantity": 10
-    }, {
-        "name": "bread",
-        "quantity": 11
-    }, {
-        "name": "eggs",
-        "quantity": 10
-    }, {
-        "name": "yogurt",
-        "quantity": 1
-    }, {
-        "name": "cartons of milk",
-        "quantity": 2
-    }, {
-        "name": "bag of coffee",
-        "quantity": 1
-    }, {
-        "name": "Easter egg from Hell",
-        "quantity": -100
-    }];
+    function ShoppingListCheckOff($scope) {
+        this.items = [{
+            "name": "cookies",
+            "quantity": 10
+        }, {
+            "name": "bread",
+            "quantity": 11
+        }, {
+            "name": "eggs",
+            "quantity": 10
+        }, {
+            "name": "yogurt",
+            "quantity": 1
+        }, {
+            "name": "cartons of milk",
+            "quantity": 2
+        }, {
+            "name": "bag of coffee",
+            "quantity": 1
+        }, {
+            "name": "Easter egg from Hell",
+            "quantity": -100
+        }];
 
-    this.bought = [];
-};
+        this.bought = [];
+    };
 
-function ToBuyShoppingController($scope, ShoppingListCheckOffService) {
-    $scope.items = ShoppingListCheckOffService.items;
-    $scope.alreadyBought = ShoppingListCheckOffService.bought;
+    function ToBuyShoppingController($scope, ShoppingListCheckOffService) {
+        $scope.items = ShoppingListCheckOffService.items;
+        $scope.alreadyBought = ShoppingListCheckOffService.bought;
 
-    $scope.buy = function(item) {
-        var newItem = item;
+        $scope.buy = function(item) {
+            var newItem = item;
 
-        $scope.alreadyBought.push(newItem);
+            $scope.alreadyBought.push(newItem);
 
-        var index = $scope.items.indexOf(item);
-        $scope.items.splice(index, 1);
+            var index = $scope.items.indexOf(item);
+            $scope.items.splice(index, 1);
 
-        ShoppingListCheckOffService.items = $scope.items;
-        ShoppingListCheckOffService.bought = $scope.alreadyBought;
-    }
-};
+            ShoppingListCheckOffService.items = $scope.items;
+            ShoppingListCheckOffService.bought = $scope.alreadyBought;
+        }
+    };
 
-function AlreadyBoughtShoppingController($scope, ShoppingListCheckOffService) {
-    $scope.alreadyBought = ShoppingListCheckOffService.bought;
-};
+    function AlreadyBoughtShoppingController($scope, ShoppingListCheckOffService) {
+        $scope.alreadyBought = ShoppingListCheckOffService.bought;
+    };
+})();
